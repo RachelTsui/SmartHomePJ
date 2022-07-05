baaea1a410418af22f8e19edc797a1bb67834258
RachelTsiu <qinzhiyun0508@gmail.com>
2022-07-05 22:33:38 +0800

    sunzewen

---
+++ login.php
@@ -0,0 +1,50 @@
+<?php
+$is_valid = false;
+if($_SERVER["REQUEST_METHOD"]=== "POST"){
+    $mysqli = require __DIR__ . "/database.php";
+    $sql = sprintf("SELECT * FROM usr
+                    WHERE email = '%s'",
+                    $mysqli->real_escape_string($_POST["email"]));
+    $result = $mysqli->query($sql);
+    $user = $result->fetch_assoc();
+
+    if($user){
+        if(password_verify($_POST["password"], $user["password_hash"])){
+            session_start();
+            session_regenerate_id();
+            $_SESSION["user_id"] = $user["id"];
+
+            header("Location: test.php");
+            exit;
+        }
+    }
+    $is_valid = true;
+}
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+</head>
+<body>
+    <h1>Login</h1>
+    <?php   if($is_valid): ?>
+        <em>Invalid login</em>
+    <?php endif; ?>
+
+    
+    <form method="post"> 
+        <label for="email">email</label>
+        <input type="email" id="email" name = "email">
+        
+        <label for="password">Password</label>
+        <input type="password" id="password" name = "password">
+        
+        <button>Login</button>
+    </form>
+    <p>Doesn't have an account? Go to <a href="./singup.html">sign up</a></p>
+</body>
+</html>
--- process-signup.php
+++ process-signup.php
@@ -1,3 +1,49 @@
 <?php
-print_r($_POST);
+if (empty($_POST["name"])){
+    die("Name is Required");
+}
+if ( ! filter_var($_POST["email"],FILTER_VALIDATE_EMAIL)){
+    die("Valid email is required");
+}
+if( (strlen($_POST["password"])<8)){
+    die("The password must contain at least 8 characters");
+}
+if(!(preg_match("/[a-z]/i", $_POST["password"]))){
+    die("Password must contain at least one letter");
+}
+if(!(preg_match("/[0-9]/", $_POST["password"]))){
+    die("Password must contain at least one number");
+}
+$password_hash = password_hash($_POST["password"],PASSWORD_DEFAULT);
+
+$mysqli = require __DIR__ . "/database.php";
+
+$sql = "INSERT INTO usr(name, email, password_hash)
+        VALUE (?,?,?)";
+
+$stmt = $mysqli->stmt_init();
+if(! $stmt->prepare($sql)){
+    die("SQL error: " . $mysqli->error);
+}
+
+$stmt->bind_param("sss",$_POST["name"],
+                        $_POST["email"],
+                        $password_hash);
+
+if ($stmt->execute()){
+    header("Location: signup-success.html");
+    exit;
+}
+else{
+    if($mysqli->errno === 1062){
+        die("email is already taken");
+    }
+    else{
+        die($mysqli->error . " " . $mysqli->errno);
+    }
+    
+}
+
+
+
 ?>
