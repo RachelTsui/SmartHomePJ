5d04585271f291a54558d097e3d510090fb5c120
Bao Xin <baoxin0905@sina.com>
2022-07-07 22:29:14 +0800

    设备添加功能

--- addeq.php
+++ addeq.php
@@ -1,40 +1,45 @@
 <?php
     $arr = array(
         '电视'=>array(),
-        '路由器'=>array('加速器','电源指示灯'),
+        'router'=>array('Accelerator','WIFI', 'Light'),
         '灯'=>array('正常','明亮','暗色')
     );
     $FamilyId = 123;
 
     $mysqli = require __DIR__ . "/database.php";
+    $con=mysqli_connect("localhost","root","000000","smarthouse");
 
-    $sql = "INSERT INTO Equipment(FamilyID, ID, Name, Cycle, Battery, Kind)
-        VALUE ('$FamilyId',?,?,?,?,?)";
+    $sql = "INSERT INTO Equipment(FamilyID, ID, Name, Date, Cycle, Battery, Kind)
+        VALUE ('$FamilyId',?,?,?,?,?,?)";
 
     $stmt = $mysqli->stmt_init();
     if(! $stmt->prepare($sql)){
         die("SQL error: " . $mysqli->error);
     }
-    $stmt->bind_param("isiis",$_POST["ID"],
+    $stmt->bind_param("issiis",$_POST["ID"],
                               $_POST["Name"],
+                              $_POST["Date"],
                               $_POST["Cycle"],
                               $_POST["Battery"],
                               $_POST["Kind"]);
-
-    if ($stmt->execute()){
-        echo "succsefful\n";
-        print_r($_POST); 
+    switch($_POST["Kind"]){
+        case "router":
+            $sql2 = "INSERT INTO router(FamilyID, ID, Pattern)
+                    VALUE ('$FamilyId', ".$_POST["ID"].",1)";
+            echo $sql2;
+            mysqli_query($con, $sql2) or die (mysqli_error());
+            break;
     }
-    else{
-        if($mysqli->errno === 1062){
-            die("Equipment id is already taken");
-        }
-        else{
-            die($mysqli->error . " " . $mysqli->errno);
-        }
-        
+    if($stmt->execute()){
+        echo"create successful!";
+    }else{
+        die($mysqli->error ." " . $mysqli->errno);
     }
+    //print_r($_POST);
+    header("Location: equipments.php");
     
+    
+    // print_r($_POST); 
 ?>
 
 
